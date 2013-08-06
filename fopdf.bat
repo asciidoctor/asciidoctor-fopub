e026d3901f4c0af0b7ed7d014aa6f83c7c3b77e5
Dan Allen <dan.j.allen@gmail.com>
2013-08-06 02:08:17 -0600

    add first cut at Windows batch script

---
+++ fopdf.bat
@@ -0,0 +1,73 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  fopdf script for Windows
+@rem
+@rem  WARNING: This script has not yet been tested!
+@rem
+@rem ##########################################################################
+
+if "%OS%"=="Windows_NT" setlocal
+
+@rem Store full-qualified drive + path of this script
+set PRG_DIR=%~dp0
+if "%PRG_DIR%" == "" set PRG_DIR=.
+
+set GRADLEW_CMD="%PRG_DIR%\gradlew"
+set FOPDF_DIR="%PRG_DIR%\build\fopdf"
+set FOPDF_CMD="%FOPDF_DIR%\bin\fopdf.bat"
+
+set DOCBOOK_DIR="%FOPDF_DIR%\docbook"
+set DOCBOOK_XSL_DIR="%FOPDF_DIR%\docbook-xsl"
+set XSLTHL_CONFIG_URI="file:///%DOCBOOK_XSL_DIR%\xslthl-config.xml"
+
+:init
+set SOURCE_FILE=
+set TYPE=pdf
+@rem Process first argument
+if %1a==a goto endInit
+@rem Store fully-qualified drive+path+name+extension of first argument
+set SOURCE_FILE=%~f1
+@rem Store fully-qualified drive+path+name of first argument
+set SOURCE_ROOTNAME=%~dpn1
+shift
+@rem Process second argument
+if %1a==a goto endInit
+set TYPE=%1
+shift
+:endInit
+
+if "%SOURCE_FILE%" == "" (
+  echo .
+  echo You must specify a DocBook XML source file as the first command argument
+  echo .
+  goto fail
+)
+
+:install
+if exist "%FOPDF_CMD%" goto endInstall
+echo .
+echo Initializing application...
+"%GRADLEW_CMD%" -q -u installApp
+if not "%ERRORLEVEL%"=="0" goto fail
+echo Application initialized!
+echo .
+:endInstall
+
+if "%TYPE%" == "pdf" (
+  set OUTPUT_PDF_FILE="%SOURCE_ROOTNAME%.pdf"
+  "%FOPDF_CMD%" -q -catalog -c "%DOCBOOK_XSL_DIR%\fop-config.xml" -xml "%SOURCE_FILE%" -xsl "%DOCBOOK_XSL_DIR%\fo-pdf.xsl" -pdf "%OUTPUT_PDF_FILE%" -param highlight.xslhl.config "%XSLTHL_CONFIG_URI%" -param admon.graphics.path "%DOCBOOK_DIR%\images\" -param callout.graphics.path "%DOCBOOK_DIR%\images\callouts\"
+  if not "%ERRORLEVEL%"=="0" goto fail else goto mainEnd
+)
+
+if "%TYPE%" == "fo" (
+  set OUTPUT_FO_FILE="%SOURCE_ROOTNAME%.fo"
+  "%FOPDF_CMD%" -q -catalog -c "%DOCBOOK_XSL_DIR%\fop-config.xml" -xml "%SOURCE_FILE%" -xsl "%DOCBOOK_XSL_DIR%\fo-pdf.xsl" -foout "%OUTPUT_FO_FILE%" -param highlight.xslhl.config "%XSLTHL_CONFIG_URI%" -param admon.graphics.path "%DOCBOOK_DIR%\images\" -param callout.graphics.path "%DOCBOOK_DIR%\images\callouts\"
+  if not "%ERRORLEVEL%"=="0" goto fail else goto mainEnd
+)
+
+:fail
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
