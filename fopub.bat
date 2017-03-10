7b888546f4d015364e7c197fdb60c5166ecf8385
Kristian Duske <kristian.duske@numberfour.eu>
2017-03-10 11:00:14 +0100

    Add JDK logging config file to reduce log output to warnings and above.

--- fopub.bat
+++ fopub.bat
@@ -62,6 +62,8 @@ set DOCBOOK_DIR_PARAM=file:///%DOCBOOK_DIR%
 set DOCBOOK_DIR_PARAM=!DOCBOOK_DIR_PARAM:\=/!
 set XSLTHL_CONFIG_URI=!XSLTHL_CONFIG_URI:\=/!
 
+SET FOPUB_OPTS="-Djava.util.logging.config.file=$APP_DIR\logging.properties"
+
 if "%TYPE%" == "pdf" (
   set OUTPUT_PDF_FILE="%SOURCE_ROOTNAME%.pdf"
   %FOPUB_CMD% -q -catalog -c "%DOCBOOK_XSL_DIR%\fop-config.xml" -xml "%SOURCE_FILE%" -xsl "%DOCBOOK_XSL_DIR%\fo-pdf.xsl" -pdf !OUTPUT_PDF_FILE! -param highlight.xslthl.config "%XSLTHL_CONFIG_URI%" -param admon.graphics.path "%DOCBOOK_DIR_PARAM%/images/" -param callout.graphics.path "%DOCBOOK_DIR_PARAM%/images/callouts/"
